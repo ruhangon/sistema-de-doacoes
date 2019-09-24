c425735ece7957f73e1e56ec909a7701627e600d
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2019-09-24 18:07:42 -0300

    Commit inicial para desenvolvimento do sistema no back end

---
+++ desenvolvimento/back-end/doe-facil/src/main/java/br/unisul/doacoes/DoeFacilApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeFacilApplication.class, args);
+	}
+
+}
---
+++ desenvolvimento/back-end/doe-facil/src/main/java/br/unisul/doacoes/domain/Usuario.java
@@ -0,0 +1,5 @@
+package br.unisul.doacoes.domain;
+
+public class Usuario {
+
+}
